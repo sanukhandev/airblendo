a2fa22b8a1489e0aca37eb46cf1580e2f873bdde
Jithin Jacob <jithinjacob@Jithins-MacBook-Pro.local>
2025-08-02 10:55:40 +0200

    flightcard update

--- src/components/FlightCard.tsx
+++ src/components/FlightCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { flights } from "../../public/datasets/places";
-import { Plane, Minus, Ellipsis } from "lucide-react";
+import { Plane, Minus, Ellipsis, Briefcase, Luggage } from "lucide-react";
 import Image from "next/image";
 import { Separator } from "@/components/ui/separator";
 
@@ -15,13 +15,13 @@ const FlightCard = () => {
               className="pointer bg-blue-900 border border-slate-200 rounded-xl shadow-md hover:shadow-lg transition-shadow"
             >
               {" "}
-              <span className="flex gap-3 items-center text-white py-1 px-3 text-xs font-bold rounded-r-lg">
-                <Plane />
+              <span className="flex gap-2 items-center text-white py-1 px-3 text-xs font-bold rounded-r-lg">
+                <Plane size={18} />
                 Direct
               </span>
-              <div className="bg-white rounded-b-xl hover:rounded-b-none shadow-md hover:-translate-y-8 transition-transform duration-500">
-                <div className="grid md:grid-cols-5 px-6 py-10 ">
-                  <div className="flex items-center col-span-1 md:border-r border-blue-900 px-2 gap-10">
+              <div className="w-full bg-white mb-[-34] rounded-b-xl hover:rounded-b-none shadow-md hover:-translate-y-8 transition-transform duration-500">
+                <div className="md:grid md:grid-cols-5 w-full">
+                  <div className="flex items-center flex-row sm:flex-col justify-center col-span-1 md:border-r border-b md:border-b-0 border-dashed border-blue-900 px-4 py-6 gap-10">
                     {flight.image ? (
                       <Image
                         src={flight.image}
@@ -37,14 +37,14 @@ const FlightCard = () => {
                         </span>
                       </div>
                     )}
-                    <div>
+                    <div className="text-center">
                       <h3 className="font-semibold text-lg">
                         {flight.airline}
                       </h3>
                       <p className="text-gray-600">{flight.number}</p>
                     </div>
                   </div>
-                  <div className="flex gap-5 items-center col-span-3 justify-center mt-5 md:mt-0">
+                  <div className="flex gap-5 items-center col-span-3 justify-center px-6 py-10  md:mt-0">
                     <span className="flex flex-col items-center">
                       <h1 className="font-bold text-blue-900">
                         {flight.departFrom}
@@ -65,20 +65,30 @@ const FlightCard = () => {
                       <h1>{flight.arriveAt}</h1>
                     </span>
                   </div>
-                  <div className="md:text-right mt-5 md:mt-0 md:text-center m-3">
-                    <p className="text-xl font-bold text-blue-900">
-                      ${flight.price}
-                    </p>
-                    {/* <p className="text-sm text-gray-500">per person</p> */}
-                    <button
-                      type="submit"
-                      className="pointer mt-3 bg-blue-950 hover:bg-white text-white hover:text-blue-900 border hover:border hover:border-blue-900 font-bold py-2 px-6 rounded-lg transition"
-                    >
-                      Book
-                    </button>
+                  <div className="text-center px-6 py-10">
+                    <>
+                      <p className="text-xl font-bold text-blue-900">
+                        ${flight.price}
+                      </p>
+                      {/* <p className="text-sm text-gray-500">per person</p> */}
+                      <button
+                        type="submit"
+                        className="pointer mt-3 bg-blue-950 hover:bg-white text-white hover:text-blue-900 border hover:border hover:border-blue-900 font-bold py-2 px-6 rounded-lg transition"
+                      >
+                        Book
+                      </button>
+                    </>
                   </div>
                 </div>
               </div>
+              <span className="flex gap-3 b-[-25] items-center text-white py-2 px-3 text-xs font-bold rounded-r-lg">
+                <>
+                  <Luggage size={18} /> 25 kg{" "}
+                </>
+                <>
+                  <Briefcase size={18} /> 7kg
+                </>
+              </span>
             </li>
           ))}
         </ul>
